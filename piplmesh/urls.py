95c381313fa6c688cc14d9655484fba44b77b9b7
administrator <administrator@johnny.(none)>
2012-03-14 18:19:24 +0100

    Removed unneeded files, added translatables, fixed indent, added local libraries.

--- piplmesh/urls.py
+++ piplmesh/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
-
 from django.contrib import admin
-admin.autodiscover()
 
 from piplmesh.frontend import views as frontend_views
 
-# import social-auth views
-from account.views import home, done, logout, error, form
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url('^$', frontend_views.HomeView.as_view(), name='home'),
@@ -19,10 +16,6 @@ urlpatterns = patterns('',
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'redirect_field_name': 'redirect_to',}, name='logout'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'redirect_field_name': 'redirect_to',}, name='login'),
 
-    # social-auth login
-    url(r'^form/$', form, name='form'),
-    url(r'^done/$', done, name='done'),
-    url(r'^error/$', error, name='error'),
-    url(r'^logout/$', logout, name='logout'),
+    # social-auth login for OpenID and facebook
     url(r'', include('social_auth.urls')),
 )
