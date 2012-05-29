ddc7d20ab493b2bdfbcb016aeb9eed7ae47b3415
agiz <ziga.zupanec@gmail.com>
2012-05-29 18:23:21 +0200

    Added Node and OCVenue mongoengine Documents.

--- piplmesh/nodes/models.py
+++ piplmesh/nodes/models.py
@@ -1,12 +1,35 @@
-class Node(object):
+import mongoengine
+
+class Node(mongoengine.Document):
+    # TODO: better id field
+    id = mongoengine.StringField(max_length=255, primary_key=True)
+    address = mongoengine.StringField(max_length=255)
+    location = mongoengine.GeoPointField()
+    name = mongoengine.StringField(max_length=150)
+    url = mongoengine.StringField(max_length=255)
+    _outside_request = mongoengine.BooleanField(default=False)
+    last_update = mongoengine.DateTimeField()
+
+class OCVenue(mongoengine.Document):
+    oc_node = mongoengine.StringField()
+    oc_address = mongoengine.StringField(max_length=255)
+    oc_distance = mongoengine.StringField(max_length=32)
+    oc_location = mongoengine.GeoPointField()
+    oc_name = mongoengine.StringField(max_length=255)
+    oc_is_open = mongoengine.BooleanField()
+    oc_timeleft = mongoengine.StringField(max_length=255)
+    oc_open_hours = mongoengine.DictField()
+    oc_extra = mongoengine.ListField()
+    oc_last_update = mongoengine.DateTimeField()
+    
+class NodeModel(object):
     def __init__(self, id, name, location, latitude, longitude, url):
         self.id = id
         self.name = name
-        self.location = location
-        self.latitude = latitude
-        self.longitude = longitude
+        self.address = location
+        self.location = [latitude, longitude]
         self.url = url
-
+        
         self._outside_request = False
 
     def is_outside_request(self):
