3f720cd3415662b014b2cd4ddb286a134791b30b
agiz <ziga.zupanec@gmail.com>
2012-05-29 18:22:43 +0200

    Changes to work with mongoengine.

--- piplmesh/nodes/backends.py
+++ piplmesh/nodes/backends.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import copy, random
 
-from . import data
+from . import data, models
 
 class RandomNodesBackend(object):
     def get_source_node(self, request):
@@ -10,8 +10,8 @@ class RandomNodesBackend(object):
         Returns a node at random.
         """
 
-        node_id = random.randrange(len(data.nodes))
-        node = copy.copy(data.nodes[node_id])
+        node_id = random.randrange(len(models.Node.objects()))
+        node = copy.copy(models.Node.objects()[node_id])
         node.id = node_id
         return node
 
@@ -21,14 +21,14 @@ class RandomNodesBackend(object):
         it just returns a random node.
         """
 
-        node_id = random.randrange(len(data.nodes))
-        node = copy.copy(data.nodes[node_id])
+        node_id = random.randrange(len(models.Node.objects()))
+        node = copy.copy(models.Node.objects()[node_id])
         node.id = node_id
         return node
 
     def get_node(self, node_id):
         try:
-            node = copy.copy(data.nodes[node_id])
+            node = copy.copy(models.Node.objects()[node_id])
             node.id = node_id
             return node
         except IndexError:
