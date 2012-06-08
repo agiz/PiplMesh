0c6a212ba2dc782fb5b28b223a4d2ad1360910a2
agiz <ziga.zupanec@gmail.com>
2012-06-08 11:43:34 +0200

    Renamed odpiralni_casi panel to openhours.

--- piplmesh/panels/openhours/panel.py
+++ piplmesh/panels/openhours/panel.py
@@ -2,11 +2,11 @@ from django.utils.translation import ugettext_lazy as _
 
 from piplmesh import panels
 
-class OdpiralniCasiPanel(panels.BasePanel):
+class OpenhoursPanel(panels.BasePanel):
     def get_context(self, context):
         context.update({
-            'header': _("OdpiralniCasi"),
+            'header': _("Openhours"),
         })
         return context
 
-panels.panels_pool.register(OdpiralniCasiPanel)
+panels.panels_pool.register(OpenhoursPanel)
