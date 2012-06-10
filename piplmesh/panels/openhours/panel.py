5e179a2bb27a5e5bf23b0311438bc3070ae14ee3
agiz <ziga.zupanec@gmail.com>
2012-06-10 10:53:27 +0200

    Added map dependency.

--- piplmesh/panels/openhours/panel.py
+++ piplmesh/panels/openhours/panel.py
@@ -3,6 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from piplmesh import panels
 
 class OpenHoursPanel(panels.BasePanel):
+    dependencies = (panels.map.panel.MapPanel.get_name(),)
     def get_context(self, context):
         context.update({
             'header': _("OpenHours"),
